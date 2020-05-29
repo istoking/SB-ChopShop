568e7f55c75b62bed06a27bb8a69b1bf4034022b
istoking <58592678+istoking@users.noreply.github.com>
2020-05-30 09:20:18 +1200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,16 @@ fx_version 'adamant'
 game 'gta5'
 
 client_scripts {
-   'client/client.lua',
-   'client/functions.lua',
-   'config.lua',
+	'client/client.lua',
+	'client/functions.lua',
+	'config.lua',
 }
 
 server_scripts {
 	'server/main.lua',
 	'config.lua',
+}
+
+dependency {
+   'mythic_notify',
 }
