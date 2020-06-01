592cd1fe603a13dc57080df904eae2d1eea0536b
istoking <58592678+istoking@users.noreply.github.com>
2020-06-01 15:00:47 +1200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -27,7 +27,7 @@ Citizen.CreateThread(function()
             if distCheck <= 25 and IsPedInAnyVehicle(ped, false) then
                 sleep = 5
                 DrawText3Ds(ChopLocation['x'],ChopLocation['y'],ChopLocation['z'],'[~g~E~s~] To Chop Vehicle')
-                DrawMarker(27, ChopLocation['x'],ChopLocation['y'],ChopLocation['z'] - 0.98, 0, 0, 0, 0, 0, 0, 3.5, 3.5, 3.0, 139, 16, 20, 250, false, false, 2, true, false, false, false)
+                DrawMarker(27, ChopLocation['x'],ChopLocation['y'],ChopLocation['z'] - 0.98, 0, 0, 0, 0, 0, 0, 3.0, 3.0, 2.5, 139, 16, 20, 250, false, false, 2, true, false, false, false)
                 if IsControlJustPressed(2, 86) then
                     local car = GetVehiclePedIsIn(ped, false)
                     local hash = GetEntityModel(car)
@@ -38,6 +38,7 @@ Citizen.CreateThread(function()
         Citizen.Wait(sleep)
     end
 end)
+
 
 RegisterNetEvent('CheckDist')
 AddEventHandler('CheckDist', function(class)
@@ -61,10 +62,12 @@ AddEventHandler('CheckDist', function(class)
 	end
 end)
 
+
 RegisterNetEvent('SB-GetList')
 AddEventHandler('SB-GetList', function()
 	GetChopList(source)
 end)
+
 
 RegisterNetEvent('Delete')
 AddEventHandler( 'Delete', function()
@@ -74,4 +77,103 @@ AddEventHandler( 'Delete', function()
         SetEntityAsMissionEntity(vehicle, true, true)
         deleteCar(vehicle)
     end 
+end)
+
+function Draw3DText(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+        
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 370
+    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
+
+        if Vdist2(coords, Config.NPCShop.x, Config.NPCShop.y, Config.NPCShop.z) <= 5 then
+            Draw3DText(Config.NPCShop.x, Config.NPCShop.y, Config.NPCShop.z + 0.5, '~w~Press [~g~E~w~] to talk to ~g~Stanley~s~')
+
+            if IsControlJustReleased(0, 38) then
+                wasOpen = true
+                OpenShop()
+            end
+        else
+            if wasOpen then
+                wasOpen = false
+                ESX.UI.Menu.CloseAll()
+            end
+
+            Citizen.Wait(500)
+        end
+    end
+end)
+
+function OpenShop()
+    local elements = {}
+    menuOpen = true
+
+    for k, v in pairs(ESX.GetPlayerData().inventory) do
+        local price = Config.itemsPrice[v.name]
+
+        if price and v.count > 0 then
+            table.insert(elements, {
+                label = ('%s - <span style="color:green;">%s</span>'):format(v.label, _U('item', ESX.Math.GroupDigits(price))),
+                name = v.name,
+                price = price,
+
+                type = 'slider',
+                value = 1,
+                min = 1,
+                max = v.count
+            })
+        end
+    end
+
+    ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'car_shop', {
+        title    = _U('shop_title'),
+        align    = 'top-right',
+        elements = elements
+    }, function(data, menu)
+        TriggerServerEvent('chopshop:sell', data.current.name, data.current.value)
+    end, function(data, menu)
+        menu.close()
+        menuOpen = false
+    end)
+end
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        if menuOpen then
+            ESX.UI.Menu.CloseAll()
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    if Config.NPCEnable == true then
+        RequestModel(Config.NPCHash)
+        while not HasModelLoaded(Config.NPCHash) do
+            Wait(1)
+        end
+        parts_dealer = CreatePed(1, Config.NPCHash, Config.NPCShop.x, Config.NPCShop.y, Config.NPCShop.z-1, Config.NPCShop.h, false, true)
+        SetBlockingOfNonTemporaryEvents(parts_dealer, true)
+        SetPedDiesWhenInjured(parts_dealer, false)
+        SetPedCanPlayAmbientAnims(parts_dealer, true)
+        SetPedCanRagdollFromPlayerImpact(parts_dealer, false)
+        SetEntityInvincible(parts_dealer, true)
+        FreezeEntityPosition(parts_dealer, true)
+        TaskStartScenarioInPlace(parts_dealer, "WORLD_HUMAN_SMOKING_POT", 0, true);
+    else
+    end
 end)
