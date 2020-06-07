372b698148830f942ff821bed36b2e7f2cec45ac
istoking <58592678+istoking@users.noreply.github.com>
2020-06-07 13:21:37 +1200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -11,7 +11,7 @@ local sr = 'stockrim'
 
 local payment = {}
 
-RegisterCommand('ChopList', function(source, args)
+RegisterCommand('choplist', function(source, args)
 	TriggerClientEvent('SB-GetList', source)
 end)
 
@@ -19,16 +19,26 @@ RegisterServerEvent('SB-ChopLow')
 AddEventHandler('SB-ChopLow', function()
 	local src = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	if xPlayer == xPlayer then
+	if xPlayer == xPlayer and Config.GiveBlack then
 		xPlayer.addInventoryItem(ab, math.random(1, 3))
 		xPlayer.addInventoryItem(bt, math.random(1, 3))
 		xPlayer.addInventoryItem(hr, math.random(1, 3))
 		xPlayer.addInventoryItem(hrim, math.random(1, 3))
 		xPlayer.addInventoryItem(lr, math.random(1, 3))
 		xPlayer.addInventoryItem(sr, math.random(1, 3))
-		payment = math.random(100, 300) 
+		local payment = math.random(100, 200) 
 		xPlayer.addAccountMoney('black_money', payment)
-		TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " dollars from that chop.", length = 5000})
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " dirty dollars from that chop.", length = 7000})
+	else
+		xPlayer.addInventoryItem(ab, math.random(1, 3))
+		xPlayer.addInventoryItem(bt, math.random(1, 3))
+		xPlayer.addInventoryItem(hr, math.random(1, 3))
+		xPlayer.addInventoryItem(hrim, math.random(1, 3))
+		xPlayer.addInventoryItem(lr, math.random(1, 3))
+		xPlayer.addInventoryItem(sr, math.random(1, 3))
+		local payment = math.random(100, 200) 
+		xPlayer.addMoney(payment)
+		TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " clean dollars from that chop.", length = 7000})
 	end
 end)
 
@@ -36,16 +46,26 @@ RegisterServerEvent('SB-ChopMed')
 AddEventHandler('SB-ChopMed', function()
 	local src = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	if xPlayer == xPlayer then
+	if xPlayer == xPlayer and Config.GiveBlack then
 		xPlayer.addInventoryItem(ab, math.random(2, 4))
 		xPlayer.addInventoryItem(bt, math.random(2, 4))
 		xPlayer.addInventoryItem(hr, math.random(2, 4))
 		xPlayer.addInventoryItem(hrim, math.random(2, 4))
 		xPlayer.addInventoryItem(lr, math.random(2, 4))
 		xPlayer.addInventoryItem(sr, math.random(2, 4))
-		payment = math.random(200, 400) 
+		local payment = math.random(200, 300) 
 		xPlayer.addAccountMoney('black_money', payment)
-		TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " dollars from that chop.", length = 5000})
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " dirty dollars from that chop.", length = 7000})
+	else
+		xPlayer.addInventoryItem(ab, math.random(2, 4))
+		xPlayer.addInventoryItem(bt, math.random(2, 4))
+		xPlayer.addInventoryItem(hr, math.random(2, 4))
+		xPlayer.addInventoryItem(hrim, math.random(2, 4))
+		xPlayer.addInventoryItem(lr, math.random(2, 4))
+		xPlayer.addInventoryItem(sr, math.random(2, 4))
+		local payment = math.random(200, 300) 
+		xPlayer.addMoney(payment)
+		TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " clean dollars from that chop.", length = 7000})
 	end
 end)
 
@@ -53,16 +73,26 @@ RegisterServerEvent('SB-ChopHigh')
 AddEventHandler('SB-ChopHigh', function()
 	local src = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	if xPlayer == xPlayer then
+	if xPlayer == xPlayer and Config.GiveBlack then
 		xPlayer.addInventoryItem(ab, math.random(3, 5))
 		xPlayer.addInventoryItem(bt, math.random(3, 5))
 		xPlayer.addInventoryItem(hr, math.random(3, 5))
 		xPlayer.addInventoryItem(hrim, math.random(3, 5))
 		xPlayer.addInventoryItem(lr, math.random(3, 5))
 		xPlayer.addInventoryItem(sr, math.random(3, 5))
-		payment = math.random(300, 500) 
+		local payment = math.random(300, 400) 
 		xPlayer.addAccountMoney('black_money', payment)
-		TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " dollars from that chop.", length = 5000})
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " dirty dollars from that chop.", length = 7000})
+	else
+		xPlayer.addInventoryItem(ab, math.random(3, 5))
+		xPlayer.addInventoryItem(bt, math.random(3, 5))
+		xPlayer.addInventoryItem(hr, math.random(3, 5))
+		xPlayer.addInventoryItem(hrim, math.random(3, 5))
+		xPlayer.addInventoryItem(lr, math.random(3, 5))
+		xPlayer.addInventoryItem(sr, math.random(3, 5))
+		local payment = math.random(300, 400) 
+		xPlayer.addMoney(payment)
+		TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " clean dollars from that chop.", length = 7000})
 	end
 end)
 
@@ -70,16 +100,26 @@ RegisterServerEvent('SB-ChopExtreme')
 AddEventHandler('SB-ChopExtreme', function()
 	local src = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	if xPlayer == xPlayer then
+	if xPlayer == xPlayer and Config.GiveBlack then
 		xPlayer.addInventoryItem(ab, math.random(4, 6))
 		xPlayer.addInventoryItem(bt, math.random(4, 6))
 		xPlayer.addInventoryItem(hr, math.random(4, 6))
 		xPlayer.addInventoryItem(hrim, math.random(4, 6))
 		xPlayer.addInventoryItem(lr, math.random(4, 6))
 		xPlayer.addInventoryItem(sr, math.random(4, 6))
-		payment = math.random(400, 600) 
+		local payment = math.random(400, 500) 
 		xPlayer.addAccountMoney('black_money', payment)
-		TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " dollars from that chop.", length = 5000})
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " dirty dollars from that chop.", length = 7000})
+	else
+		xPlayer.addInventoryItem(ab, math.random(4, 6))
+		xPlayer.addInventoryItem(bt, math.random(4, 6))
+		xPlayer.addInventoryItem(hr, math.random(4, 6))
+		xPlayer.addInventoryItem(hrim, math.random(4, 6))
+		xPlayer.addInventoryItem(lr, math.random(4, 6))
+		xPlayer.addInventoryItem(sr, math.random(4, 6))
+		local payment = math.random(400, 500) 
+		xPlayer.addMoney(payment)
+		TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = "You just made $" .. payment .. " clean dollars from that chop.", length = 7000})
 	end
 end)
 
@@ -89,14 +129,12 @@ AddEventHandler('chopshop:sell', function(itemName, amount)
 	local price = Config.itemsPrice[itemName]
 	local xItem = xPlayer.getInventoryItem(itemName)
 
-
 	if xItem.count < amount then
 		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = _U('not_enough'), length = 5000})
 		return
 	end
 
 	price = ESX.Math.Round(price * amount)
-
 	if Config.GiveBlack then
 		xPlayer.addAccountMoney('black_money', price)
 	else
@@ -104,6 +142,5 @@ AddEventHandler('chopshop:sell', function(itemName, amount)
 	end
 
 	xPlayer.removeInventoryItem(xItem.name, amount)
-
 	TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = _U('sold', amount, xItem.label, ESX.Math.GroupDigits(price)), length = 5000})
 end)
